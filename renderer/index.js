1e4d7dcefede5cd552e123574cd33a6bebdc6f8c
j-meyer-25 <j-meyer.13@onu.edu>
2025-02-05 10:06:10 -0500

    Update index.js
    remove unneeded lines

--- renderer/index.js
+++ renderer/index.js
@@ -2,16 +2,12 @@
 document.getElementById('searchButton').addEventListener('click', async () => {
   const data = await window.electronAPI.getData();
   console.log(data);
-  var table = document.getElementById('dataTable')
+  var table = document.getElementById('dataTable');
   var i = 0;
   console.log(data);
   for (const [key, value] of Object.entries(data.recordsets[0][0])) {
-   // console.log(`${key}: ${value}`);
-    table.rows[1].cells[i].innerHTML = value
+    // console.log(`${key}: ${value}`);
+    table.rows[1].cells[i].innerHTML = value;
     i++;
   }
-  for (var i = 0, cell; cell = table.rows[1].cells[i]; i++) {
-   // cell.innerHTML = data.recordsets[0][0];
-  }
-  
 });

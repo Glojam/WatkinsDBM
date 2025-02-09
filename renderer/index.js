7e99e8bddf67c7a2a9746dce574486eec9bf0d0a
tangocharlie44 <128840505+tangocharlie44@users.noreply.github.com>
2025-02-09 14:52:39 -0500

    Added listener for update button. Selects file, does not upload yet

--- renderer/index.js
+++ renderer/index.js
@@ -1,4 +1,5 @@
-// VERY TEMPORARY!
+// VERY TEMPORARY
+//listener and actions for search button
 document.getElementById('searchButton').addEventListener('click', async () => {
   const data = await window.electronAPI.getData();
   console.log(data);
@@ -11,3 +12,26 @@ document.getElementById('searchButton').addEventListener('click', async () => {
     i++;
   }
 });
+// listener and action for button to upload files to database
+document.getElementById('update').addEventListener('click', async () => {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = "text/plain";
+  input.style.display = "none";
+  input.addEventListener("change", function () {
+      if (input.files.length > 0) {
+          alert("Selected file: " + input.files[0].name);
+          console.log(input.files[0].path)
+          handleUpload(input.files[0].path)
+      }
+  });
+  document.body.appendChild(input);
+  input.click();
+  document.body.removeChild(input);
+  console.log(input);
+});
+async function handleUpload(input) {
+  const filePath = input; // Replace with actual file input
+  const response = await window.electronAPI.upload(filePath);
+  console.log(response);
+}

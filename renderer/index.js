08e616602e47b22c6fc0bfa75c6c357a9ed296bd
glaywrestling <g-layman.1@onu.edu>
2025-02-12 11:03:05 -0500

    Help button functionality and initial guide

--- renderer/index.js
+++ renderer/index.js
@@ -34,6 +34,27 @@ document.getElementById("searchButton").addEventListener("click", async () => {
   });
 });
 // listener and action for button to upload files to database
+document.getElementById("helpButton").addEventListener('click', async () => {
+  alert(
+    "Search Fields: Used to search for specific lines of data that have the matching criteria specified or can be left blank to display all last names\n" + 
+    "i.e. Only the search fields that you have specified values within will be search upon & highlighted\n" +
+    "ex. Inputing \"John\" in the First Name search field and selecting the year range \"2023-2024\" will show all data records having the first name of John in the 2023-2024 season.\n" +
+    "---------------\n" +
+    "Search Button: Displays database entries into table based on inputs\n" +
+    "---------------\n" +
+    "Help Button: Displays this popup with descriptions of the app's interface\n" +
+    "---------------\n" +
+    "Switch Table: Displays a popup where you can choose a different table to display. Options are:\n" +
+    "• Players (stats about offensive & defensive players in a single game)\n" +
+    "• Goalkeepers (stats about goalkeepers in a single game)\n" +
+    "• Players Total (totals stats for offensive & defensive players in a season)\n" +
+    "• Goalkeepers Total (totals stats for goalkeepers in a season)\n" +
+    "• Team Record (general details regarding each game played [score, outcome, etc.])\n" +
+    "---------------\n" +
+    "Upload File: Used to select a MaxPreps file whose data values will be added into the database"
+  );
+});
+// listener and action for button to upload files to database
 document.getElementById('update').addEventListener('click', async () => {
   handleUpload()
 });
